f4838167f94825e034c24e684e5583c5084c82ab
runneradmin <runneradmin@fv-az1400-218>
2024-09-07 02:04:02 +0000

    Add project files.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var task = client.GetStringAsync($"https://google.com");
+            int a = 0;
+            for(int i = 0;i<1_000_000;i++)
+            {
+                a = i + 1;
+            }
+            var page = await task;
+            Console.WriteLine("Hello World");
+        }
+    }
+}
